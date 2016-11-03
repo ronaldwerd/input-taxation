bb20b761cbb795881b5c034c23bbcb336704a483
Ronald Partridge <rond@ronald-douglas.com>
2016-11-03 17:22:37 -0400

    First commit

---
+++ src/main/java/com/app/rdc/taxation/Application.java
@@ -0,0 +1,7 @@
+package com.app.rdc.taxation;
+
+/**
+ * Created by Ronald on 11/3/2016.
+ */
+public class Application {
+}
---
+++ src/main/java/com/app/rdc/taxation/Main.java
@@ -0,0 +1,20 @@
+package com.app.rdc.taxation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        while(1 == 1) {
+
+            String input = userInput.nextLine().trim();
+
+            System.out.println("you said: " + input);
+        }
+    }
+}
