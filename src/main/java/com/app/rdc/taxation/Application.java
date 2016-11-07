5da91d1138a5e3f5ab2bf710802bcce0eb144cb8
Ronald Partridge <rond@ronald-douglas.com>
2016-11-06 20:20:18 -0500

    Removed useless comments from Application.java

--- src/main/java/com/app/rdc/taxation/Application.java
+++ src/main/java/com/app/rdc/taxation/Application.java
@@ -51,13 +51,6 @@ public class Application {
     }
 
     private void renderOrder(Order o) {
-        /*
-        1 book : 12.49
-        1 music CD: 16.49
-        1 chocolate bar: 0.85
-        Sales Taxes: 1.50
-        Total: 29.83
-        */
 
         if(o.getOrderLines().size() == 0) {
             System.out.println(ORDER_EMPTY);
